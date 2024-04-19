13b017ae041fd021929fcc5661bc0906248ea310
lwieckowski <lwieckowski@hotmail.com>
2024-04-19 15:32:20 +0200

    Include alpine

--- app/rollup.config.mjs
+++ app/rollup.config.mjs
@@ -1,12 +1,14 @@
-import babel from '@rollup/plugin-babel';
+import babel from "@rollup/plugin-babel";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import terser from '@rollup/plugin-terser';
 
 const config = {
-  input: 'src/static/js/main.js',
+  input: "src/static/js/main.js",
   output: {
-    file: 'src/static/dist/bundle.js',
-    format: 'es'
+    file: "src/static/dist/bundle.js",
+    format: "es",
   },
-  plugins: [babel({ babelHelpers: 'bundled' })]
+  plugins: [babel({ babelHelpers: "bundled" }), nodeResolve(), terser()],
 };
 
 export default config;

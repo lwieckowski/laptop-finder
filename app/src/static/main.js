304ff768ef93c53630a4dcdeab4db0b54d61eb04
lwieckowski <lwieckowski@hotmail.com>
2024-04-20 11:26:38 +0200

    Add basic structure

---
+++ app/src/static/main.js
@@ -0,0 +1,29 @@
+const filterOpen = document.getElementById("filter-open");
+const filterClose = document.getElementById("filter-close");
+const filterOffcanvas = document.getElementById("filter-offcanvas");
+const sortToggle = document.getElementById("sort-toggle");
+const sortMenu = document.getElementById("sort-menu");
+
+const accordions = document.getElementsByClassName("accordion");
+
+for (let i = 0; i < accordions.length; i++) {
+  accordions[i].addEventListener("click", () => {
+    accordions[i].nextElementSibling.classList.toggle("hidden");
+    const icons = accordions[i].getElementsByClassName("icon");
+    for (let j = 0; j < icons.length; j++) {
+      icons[j].classList.toggle("hidden");
+    }
+  });
+}
+
+filterOpen.addEventListener("click", () => {
+  filterOffcanvas.classList.remove("hidden");
+});
+
+filterClose.addEventListener("click", () => {
+  filterOffcanvas.classList.add("hidden");
+});
+
+sortToggle.addEventListener("click", () => {
+  sortMenu.classList.toggle("hidden");
+});
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,jinja}"],
   theme: {
     extend: {},
   },

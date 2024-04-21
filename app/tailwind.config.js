4f3e52e77edc5279ec3902e2f2d3ce23633a2acb
lwieckowski <lwieckowski@outlook.com>
2024-04-21 09:53:15 +0200

    Add flowbite

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,jinja}"],
+  content: [
+    "./src/**/*.{html,js,jinja}",
+    "./node_modules/flowbite/**/*.js"
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("flowbite/plugin")
+  ],
 }
 
